7d385a6e5b1b2c1fd60f9506fc9f27d356caa224
Albin Bergström <albinbergstrom@gmail.com>
2015-09-28 13:53:05 +0200

    Småfix

--- app/src/main/java/applications/the4casters/microphonemodifier/effects/Bandpass.java
+++ app/src/main/java/applications/the4casters/microphonemodifier/effects/Bandpass.java
@@ -4,8 +4,8 @@ package applications.the4casters.microphonemodifier.effects;
  * Created by filles-dator on 2015-09-28.
  */
 public class Bandpass extends AudioEffect {
-    private double low_pass;
-    private double high_pass;
+    private int low_pass;
+    private int high_pass;
 
     public Bandpass(){
         effectType = AudioEffect.BANDPASS;
@@ -17,14 +17,14 @@ public class Bandpass extends AudioEffect {
     public void setLowPass(int low_pass){ this.low_pass = low_pass; }
     public void setHighPass(int high_pass) { this.high_pass = high_pass;}
 
-    public double getLowPass() { return low_pass;}
-    public double getHighPass() { return high_pass; }
+    public int getLowPass() { return low_pass;}
+    public int getHighPass() { return high_pass; }
 
     @Override
     public double[] runEffect(double[] fft) {
         for(int i = 0; i < fft.length; i++)
         {
-            if(i/fft.length < low_pass && i/fft.length > high_pass)
+            if(i < low_pass && i > high_pass)
                 fft[i] = 0;
         }
         return fft;
